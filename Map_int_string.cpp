0b98b1c63087cfa83d9de1444130e2c5ae933c3c
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-05-15 01:24:32 +0600

    M2 Map int to string
    if value not found then print blank string that's it

---
+++ Map_int_string.cpp
@@ -0,0 +1,45 @@
+/* Author: Code__R
+   Email: mondalroudra212@gmail.com
+   Date  : 05 / 15 / 2025
+   Time  : 01:20
+   Problem Link: 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl '\n'
+#define ld long double
+#define pb push_back
+#define po pop_back
+#define mx max()
+#define min min()
+#define all(v) v.begin(), v.end()
+#define F first
+#define S second
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+const ld PI = acos(-1.0);
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL);
+void solve() {
+    // Your code here
+    map<int, string> v;
+    v[1] = "Roudra";
+    v[2] = "Pri";
+    v[3];
+    cout << v[1] << nl << v[2] << nl << v[3] << nl;
+    
+}
+
+int main() {
+    fast_io;
+    int t ;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
