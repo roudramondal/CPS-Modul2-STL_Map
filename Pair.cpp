73aa62a8bcbeac96e21bed97ad58d2afd0de9df5
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-05-15 00:07:07 +0600

    Pair use in c++
    pair<string,int>v{"Roudra",1055} call it v.first and v.second

---
+++ Pair.cpp
@@ -0,0 +1,44 @@
+/* Author: Code__R
+   Email: mondalroudra212@gmail.com
+   Date  : 05 / 14 / 2025
+   Time  : 23:52
+   Problem Link: 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl '\n'
+#define ld long double
+#define pb push_back
+#define po pop_back
+#define mx max()
+#define min min()
+#define all(v) v.begin(), v.end()
+#define F first
+#define S second
+#define co cout
+#define c cin
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+const ld PI = acos(-1.0);
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL);
+void solve() {
+    // Your code here
+    pair<string, int>v = {"Roudra", 1055};
+    cout<< v.first << " ";
+    cout<< v.second << nl;
+}
+
+int main() {
+    fast_io;
+    int t ;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
