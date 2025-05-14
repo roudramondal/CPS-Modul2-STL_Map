0bcb444ae09f7219390afd332f76e52c9a3235b8
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-05-15 00:53:12 +0600

    M2 Map in STL
    map<char,int>mp;
    mp['A'] =1 so print here 1

---
+++ Map.cpp
@@ -0,0 +1,42 @@
+/* Author: Code__R
+   Email: mondalroudra212@gmail.com
+   Date  : 05 / 15 / 2025
+   Time  : 00:49
+   Problem Link: 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl '\n'
+#define ld long double
+#define pb push_back
+#define po pop_back
+#define mx max()
+#define min min()
+#define all(v) v.begin(), v.end()
+#define F first
+#define S second
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+const ld PI = acos(-1.0);
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL);
+void solve() {
+    // Your code here
+    map<char, int> mp;
+    mp['A'] = 1;
+    cout << mp['A'] << nl;
+}
+
+int main() {
+    fast_io;
+    int t ;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
