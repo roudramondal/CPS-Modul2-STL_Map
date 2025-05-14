dd49f5aea087aee493bc6ae85fd5c9f031abe5ad
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-05-15 01:09:15 +0600

    M2 Map in stl
    if  value not assign in the key then output will be 0

---
+++ value_find.cpp
@@ -0,0 +1,46 @@
+/* Author: Code__R
+   Email: mondalroudra212@gmail.com
+   Date  : 05 / 15 / 2025
+   Time  : 01:03
+   Problem Link: 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl '\n'
+#define ld long double
+#define pb push_back
+#define po pop_back
+#define mx max()
+#define min min()
+#define all(v) v.begin(), v.end()
+#define F first
+#define S second
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+const ld PI = acos(-1.0);
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL);
+void solve() {
+    // Your code here
+    map<string, int> v;
+    v["Roudra"] = 1055;
+    v["pri"] = 1056;
+    v["find"];
+    cout << v["Roudra"] << nl;
+    cout << v["pri"] << nl;
+    cout << v["find"] << nl;
+}
+
+int main() {
+    fast_io;
+    int t ;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
