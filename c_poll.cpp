e24e98e66cf2b778005dbe58101d5709ac47747e
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-05-15 19:28:06 +0600

    M2 At Coder problem

---
+++ c_poll.cpp
@@ -0,0 +1,65 @@
+/* Author: Code__R
+   Email: mondalroudra212@gmail.com
+   Date  : 05 / 15 / 2025
+   Time  : 19:19
+   Problem Link: 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl '\n'
+#define ld long double
+#define pb push_back
+#define po pop_back
+#define mx max()
+#define min min()
+#define all(v) v.begin(), v.end()
+#define F first
+#define S second
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+const ld PI = acos(-1.0);
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL);
+void solve() {
+    // Your code here
+    int n;
+    cin >> n;
+    map<string, int> votes;
+    for (int i = 0; i < n;i++)
+    {
+      string s;
+      cin >> s;
+
+      votes[s]++;
+    }
+    int max = 0;
+    for (pair<string,int> a : votes)
+    {
+      if (a.second>max)
+      {
+        max = a.second;
+      }
+    }
+    for (pair<string,int> a : votes)
+    {
+      if (a.second==max)
+      {
+        cout <<a.first << nl;
+       // return;
+      }
+    }
+}
+
+int main() {
+    fast_io;
+    // int t ;
+    // cin >> t;
+    // while (t--) {
+        solve();
+    //}
+    return 0;
+}
